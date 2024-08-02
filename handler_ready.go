567da9e7ac4973a6bf832df2aff9e0b7df94379e
ArrayOfLilly <arrayoflilly@gmail.com>
2024-08-02 06:25:53 +0200

    metrics, reset metrics

---
+++ handler_ready.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handlerReady(w http.ResponseWriter, r *http.Request) {
+	// add key, value to the header
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	// set the status code
+	w.WriteHeader(http.StatusOK)
+	// writes the response body in byte slice
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerErr(w http.ResponseWriter, r *http.Request) {
+	// add key, value to the header
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	// set the status code
+	w.WriteHeader(http.StatusInternalServerError)
+	// writes the response body in byte slice
+	w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+}
--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	// baseUrl = "."
+// a struct that will hold any stateful, in-memory data we'll need to keep track of
+type apiConfig struct {
+	fileserverHits int
+}
 
-	
+func main() {
+	baseUrl := "."
+	apiCfg := apiConfig{}
+
   	if err := godotenv.Load(); err != nil {
     	log.Fatal("Error loading .env file")
   	}
@@ -21,16 +26,49 @@ func main() {
 	// ServeMux is an HTTP request multiplexer. 
 	// It matches the URL of each incoming request against a list of registered patterns and 
 	// calls the handler for the pattern that most closely matches the URL.
+	// The ServeMux is an http.Handler.
 	mux := http.NewServeMux()
 
+	// binds a handler against the route
+	// basic handler,  which simply returns a built-in http.Handler
+	
+	// An http.Handler is just an interface: 
+	// type Handler interface {
+	// 	ServeHTTP(ResponseWriter, *Request)
+	// }
+
+	// FileServer returns a handler that serves HTTP requests with the contents of the file system rooted at root.
+	
+	// To serve a directory on disk (/) under an alternate URL
+	// path (/app), use StripPrefix to modify the request
+	// URL's path before the FileServer sees it:
+	mux.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(baseUrl)))))
+
+	
+	// http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/tmp"))))
+
+	// type HandlerFunc func(ResponseWriter, *Request)
+	// The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers. 
+	// If f is a function with the appropriate signature, HandlerFunc(f) is a Handler that calls f.
+	// func (f HandlerFunc) ServeHTTP(w ResponseWriter, r *Request)
+	// ServeHTTP calls f(w, r).
+	mux.HandleFunc("GET /api/healthz", handlerReady)
+	mux.HandleFunc("GET /api/error", handlerErr)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	
+	
 	// A Server defines parameters for running an HTTP server. 
 	// The zero value for Server is a valid configuration.
-	srv := http.Server{
+	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
 
 	// ListenAndServe listens on the TCP network address srv.Addr and 
 	// then calls Serve to handle requests on incoming connections. 
+	// opens a TCP socket
 	log.Fatal(srv.ListenAndServe())
 }
